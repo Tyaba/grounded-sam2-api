b36185038e5415fc4f4d3564830817b624c75ea0
Tyaba <tepei@keio.jp>
2024-10-18 03:19:48 +0000

    chore(user_interface): log request receive & complete

--- src/user_interface/grounded_sam.py
+++ src/user_interface/grounded_sam.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel, ConfigDict, Field
 
 from src.usecase.grounded_sam import GroundedSAM
 from src.utils.image import base642pil
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class SegmentReuqest(BaseModel):
@@ -23,9 +26,11 @@ class SegmentUserInterface:
         self.use_case = use_case
 
     def segment(self, request: SegmentReuqest) -> SegmentResponse:
+        logger.info("segment request received")
         image = base642pil(image_base64=request.image)
         sam2_output, gdino_output, _ = self.use_case.segment(
             image=image,
             text=request.text,
         )
+        logger.info("segment done")
         return SegmentResponse(masks=sam2_output.mask_arrays.tolist())
